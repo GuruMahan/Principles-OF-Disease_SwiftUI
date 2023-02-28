e79130aeb355594908d5b28b0ba793db6607b027
Hariprasath <hariprasath3456@gmail.com>
2023-02-28 13:22:34 +0530

    Initial Commit

---
+++ Principles OF Disease XIB POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Principles OF Disease XIB POC
+//
+//  Created by Guru Mahan on 28/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
